c94abefae86720e905c2acc9169b70ebba319ae6
Rafki <74223938+osd0081@users.noreply.github.com>
2022-05-15 13:28:28 +0700

    feat: config now with .env

--- config/config.go
+++ config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"sync"
 
 	"github.com/labstack/gommon/log"
@@ -10,20 +11,20 @@ import (
 
 type AppConfig struct {
 	App struct {
-		Port   int    `toml:"port"`
-		JWTKey string `toml:"jwtkey"`
+		Port   int    `toml:"port" mapstructure:"port"`
+		JWTKey string `toml:"jwtkey" mapstructure:"jwtkey"`
 	} `toml:"app"`
 	Database struct {
-		Driver string `toml:"driver"`
-		DBURL  string `toml:"dburl"`
+		Driver string `toml:"driver" mapstructure:"driver"`
+		DBURL  string `toml:"dburl" mapstructure:"dburl"`
 	} `toml:"database"`
 	Log struct {
-		Driver string `toml:"driver"`
+		Driver string `toml:"driver" mapstructure:"driver"`
 	} `toml:"log"`
 	OpenApi struct {
-		MusixMatch    string `toml:"musixmatch"`
-		MusixMatchUrl string `toml:"musixmatchurl"`
-	} `toml:"musixmatch"`
+		MusixMatch    string `toml:"musixmatch" mapstructure:"musixmatch"`
+		MusixMatchUrl string `toml:"musixmatchurl" mapstructure:"musixmatchurl"`
+	} `toml:"openapi"`
 }
 
 var lock = &sync.Mutex{}
@@ -44,8 +45,8 @@ func initConfig() *AppConfig {
 	var defaultConfig AppConfig
 	defaultConfig.App.Port = 5006
 
-	viper.SetConfigType("toml")
-	viper.SetConfigName("config")
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
 	viper.AddConfigPath("./config/")
 
 	if err := viper.ReadInConfig(); err != nil {
@@ -59,5 +60,6 @@ func initConfig() *AppConfig {
 		log.Info("error when parse config file", err)
 		return &defaultConfig
 	}
+	fmt.Println(finalConfig)
 	return &finalConfig
 }
